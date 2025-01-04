aea376169e08816005972fb392c977440b0151fd
Ava Tuttle <Travis.Tuttle@sdgku.edu>
2025-01-04 09:53:56 -0700

    message board

---
+++ posts/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+class Post(models.Model):
+    title = models.CharField(max_length=128)                #inheritance
+    subtitle = models.CharField(max_length=256)             #composition
+    body = models.TextField()                               #composition
+    created_on = models.DateTimeField(auto_now_add=True)    #composition
+
+    def __str__(self):
+        return self.title
